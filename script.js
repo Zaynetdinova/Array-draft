0b5dbb63996c09bdb474b81cccb30a29006b6baa
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2022-05-25 22:24:58 +0500

    add

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('ilmira');
+let str = "stringify";
+// 'strin', символы от 0 до 5 (не включая 5)
+alert( str.slice(0, 5) );
+// 's', от 0 до 1, не включая 1, т. е. только один символ на позиции 0
+alert( str.slice(0, 1) );
